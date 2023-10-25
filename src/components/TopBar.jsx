8d4435e3d58922bf26c5c472c9f56e2459196dd6
Michael Emmanuel <emmanuelojonugwa009@gmail.com>
2023-10-25 04:56:38 +0100

    created topbar component

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -6,14 +6,14 @@ import { Link } from "react-router-dom"
 const TopBar = () =>
 {
     return (
-        <div className="w-full px-3">
+        <div className="w-full px-3 bg-[#202020]">
             <div className="flex  flex-row relative content-center items-center justify-center py-3 ">
-                <div>
-                    <input className="inline-block border-none outline-none h-10 w-[400px] bg-red-400" type="text" name="" id="" placeholder="search.." />
-                    <SearchOutlinedIcon />
+                <div className='border  rounded  border-solid  pl-4'>
+                    <input className="inline-block border-none outline-none text-white h-10 w-[350px] bg-transparent" type="text" name="" id="" placeholder="search.." />
+                    <SearchOutlinedIcon className='text-[#3d3a3a]'/>
                 </div>
-                <div>
-                    <Link className="absolute -right-0" to="/signin">
+                <div className='absolute -right-0'>
+                    <Link className=" inline-block h-8 text-white flex gap-1 border rounded-sm border-solid p-1 capitalize" to="/signin">
                     signin
                         <AccountCircleOutlinedIcon />
                     </Link>
