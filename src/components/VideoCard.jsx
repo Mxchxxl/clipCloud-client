e7282fa739395e2ff9990fa00c9c25b5ef0b40f2
Michael Emmanuel <emmanuelojonugwa009@gmail.com>
2023-10-26 06:12:42 +0100

    styled video card component

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,19 +1,20 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
-const VideoCard = ()=>{
+const VideoCard = () =>
+{
     return (
-        <Link className="no-underline ">
-<div>
-    <img src="" alt="" />
-    <div>
-        <img src="" alt="" />
-        <div >
-            <span>test video</span>
-            <span>michael</span>
-                <div><span>1 view</span> &middot; <span>2 mins ago</span></div>
-        </div>
-    </div>
-</div>
+        <Link className="no-underline inline-block  m-2 max-w-[288px]">
+            <div> 
+                <img src="" alt=""  className="w-72 h-44 "/>
+                <div className="mt-3 grid grid-cols-8 w-full">
+                    <img className="w-8 h-8" src="" alt="" />
+                    <div className="col-start-2 col-span-6 ml-3 flex flex-col">
+                        <span className="font-semibold text-base capitalize ">test video</span>
+                        <span className="text-gray-700 capitalize text-sm">michael</span>
+                        <div className="text-xs"><span>1 view</span> &middot; <span>2 mins ago</span></div>
+                    </div>
+                </div>
+            </div>
         </Link>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () =>
     return (
 
 
-        <div>Home
+        <div >
 {
     dumyArray.map(object=>
         <VideoCard />
